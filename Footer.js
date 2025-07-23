20ff7ac41ae5c0c95089abf3ef8099ce4ea0e7c6
mmotal79 <82840460+mmotal79@users.noreply.github.com>
2025-07-22 23:40:14 -0400

    Update Footer.js

--- Footer.js
+++ Footer.js
@@ -1,14 +1,1 @@
-import React from 'react';
 
-function Footer() {
-  return (
-    <footer className="bg-gray-800 text-white py-8 text-center text-sm">
-      <div className="container mx-auto px-6">
-        <p>&copy; {new Date().getFullYear()} Miguel Mota. Todos los derechos reservados.</p>
-        <p className="mt-2">Diseñado y desarrollado por: Miguel Mota Architect of Technological Solutions.</p>
-      </div>
-    </footer>
-  );
-}
-
-export default Footer;
