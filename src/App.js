89121a6d4affa16fdc8ccf82ca5bf8ad996ec4b0
mmotal79 <82840460+mmotal79@users.noreply.github.com>
2025-07-24 09:05:16 -0400

    Update App.js corregido
    elimina ducplicidad de creacion del agente ia

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HeroSection from './components/HeroSection';
 import AboutMe from './components/AboutMe';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
-import AIAgent from './components/AIAgent'; // ¡Añadido aquí!
+import AIAgent from './components/AIAgent'; // 
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Header from './components/Header.js';
@@ -13,19 +13,15 @@ import Header from './components/Header.js';
 function App() {
   return (
     <div className="min-h-screen bg-gray-50 font-sans antialiased">
-	<Header /> {/* Agrega el Header aquí, antes de todas las secciones */}
+    <Header /> {/* Agrega el Header aquí, antes de todas las secciones */}
     <main>
       <HeroSection />
       <AboutMe />
       <Skills />
       <Projects />
-      <section id="ia-agent" className="py-16 md:py-24 bg-gray-50">
-          <div className="container mx-auto px-6">
-            <AIAgent /> {/* Solo una instancia de AIAgent aquí */}
-          </div>
-        </section>
+      <AIAgent /> 
+      <Contact />	
     </main>
-	<Contact />	
     <Footer />
     </div>
   );
