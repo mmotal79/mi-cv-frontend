f95eb5b890489df5ba6955035eb15163e77f31c8
mmotal79 <82840460+mmotal79@users.noreply.github.com>
2025-07-29 12:08:40 -0400

    Update Footer.js ajustes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,13 @@
+// src/components/Footer.js
 import React from 'react';
 
 function Footer() {
   return (
-    <footer className="bg-gray-800 text-white py-8 text-center text-sm">
+    // CAMBIO: Fondo a bg-black, texto a text-base y font-bold para resaltar
+    <footer className="bg-black text-white py-8 text-center text-base font-bold">
       <div className="container mx-auto px-6">
-        <p>&copy; {new Date().getFullYear()} Miguel Mota. Todos los derechos reservados.</p>
-        <p className="mt-2">Diseñado y desarrollado por: Miguel Mota Architect of Technological Solutions.</p>
+        <p>&copy; {new Date().getFullYear()} Ing. Miguel Mota. Todos los derechos reservados.</p>
+        <p className="mt-2">Diseñado y desarrollado por: Ing. Miguel Mota, Project Manager & Architect of Technological Solutions.</p>
       </div>
     </footer>
   );
